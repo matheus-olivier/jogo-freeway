5090254a16509fb8c0768a1f0442dfc9e8787a8b
Theus <106281465+matheus-olivier@users.noreply.github.com>
2023-07-10 15:36:00 -0300

    divisão
    dividindo os códigos em arquivos separados para melhor organização.

---
+++ imagens.js
@@ -0,0 +1,11 @@
+//imagens do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor; 
+let imagemCarro; 
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png")
+  imagemDoAtor = loadImage("imagens/ator-1.png")
+  imagemCarro = loadImage("imagens/carro-1.png");
+}
