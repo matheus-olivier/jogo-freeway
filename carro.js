393ce15d2d8610e7dc916798c1620c6e3dfee086
Theus <106281465+matheus-olivier@users.noreply.github.com>
2023-07-11 11:03:15 -0300

    laços
    simplificando código com laços de repetição

--- carro.js
+++ carro.js
@@ -5,9 +5,9 @@ let yCarros = [40, 96, 150];
 let velocidadeCarros =[2, 2.5, 3.2];
 
 function mostraCarro(){
-  image(imagemCarro, xCarros[0], yCarros[0], 50, 40);
-  image(imagemCarro2, xCarros[1], yCarros[1], 50, 40);
-  image(imagemCarro3, xCarros[2], yCarros[2], 50, 40);
+  for(let i = 0; i < imagemCarros.length; i = i + 1){
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
+  }
 }
 
 function movimentaCarro(){
